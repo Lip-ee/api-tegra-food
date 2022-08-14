c99fa3761513e6f30237165d940561b1b6b02190
Filipe Campos <lip3.campos@gmail.com>
2022-08-13 23:52:48 -0300

    filtering system done (working) + new roter (get filtering)

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
     deleteProductById,
     findAllProducts,
+    findAllProductsWithFilter,
     findProductById,
     pingPong,
     saveProducts,
@@ -13,8 +14,9 @@ const router = Router();
 router.get('/api-tegra/ping', pingPong); // ping pong
 
 /*   ENDPOINTS   */ 
-router.post('/api-tegra/', saveProducts); // save products
-router.get('/api-tegra/', findAllProducts); // find all products
+router.post('/api-tegra', saveProducts); // save products
+router.get('/api-tegra', findAllProducts); // find all products
+router.get('/api-tegra', findAllProductsWithFilter); // find especific products
 router.get('/api-tegra/:id', findProductById); // find products by id
 router.put('/api-tegra/:id', updateProductById); // update products by id
 router.delete('/api-tegra/:id', deleteProductById); // delete products by id
